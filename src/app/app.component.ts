d359c15c28e173494eb7e6404401c139deb5229b
Alan Ng <alan.ng@blake.com.au>
2019-06-16 19:13:52 +1000

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,33 @@ import { Product } from './product.model';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  product: Product;
+  products: Product[];
 
   constructor() {
-    let newProduct = new Product(
-      'NICEHAT',
-      'A Nice Blake Hat',
-      '/resources/images/products/black-hat.jpg',
-      ['Men', 'Accessories', 'Hats'],
-      29.99
-    );
+    this.products = [
+      new Product(
+        'MYSHOES',
+        'Black Running Shoes',
+        '/assets/images/products/black-shoes.jpg',
+        ['Men', 'Shoes', 'Running Shoes'],
+        109.99),
+      new Product(
+        'NEATOJACKET',
+        'Blue Jacket',
+        '/assets/images/products/blue-jacket.jpg',
+        ['Women', 'Apparel', 'Jackets & Vests'],
+        238.99),
+      new Product(
+        'NICEHAT',
+        'A Nice Black Hat',
+        '/assets/images/products/black-hat.jpg',
+        ['Men', 'Accessories', 'Hats'],
+        29.99)
+      ];
+  }
 
-    this.product = newProduct;
+  productWasSelected(product: Product): void {
+    console.log('Product clicked: ', product);
   }
 
 }
